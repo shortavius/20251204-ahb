0da46c83bfacfea026a47fd7497c38bf9545b454
Jason Melton <jmelton@jasonmelton.com>
2025-11-19 13:23:55 -0500

    Add blank Arduino project

---
+++ demo/demo.ino
@@ -0,0 +1,11 @@
+// This function is used to initialize the various hardware that will be used
+// in the code. It is only called once.
+void setup()
+{
+}
+
+// This function is where the actual application code resides. It will run thru
+// the code to the bottom and then start back over from the top forever.
+void loop()
+{
+}
